c6dcac355c2258f15a49015770ba7f929a09d178
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-10 22:33:52 +0530

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,11 @@
+public class AA 
+{
+	public AA()
+	{
+		System.out.println("Constructor is working fine");
+	}
+	static void Metho() 
+	{
+		System.out.println("Method is working fine");
+	}
+}
---
+++ AAK2.java
@@ -0,0 +1,10 @@
+public class AAK2 
+{
+	public static void main(String[] args) 
+	{
+		new AA();
+		AA.Metho();
+		
+	}
+
+}
