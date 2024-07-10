f6f9f5b95f76d910e259b01dcf32eeaedba3e53b
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-10 22:39:56 +0530

    Add files via upload

---
+++ AA1.java
@@ -0,0 +1,16 @@
+public class AA1 
+{
+	public AA1()
+	{
+		System.out.println("Without parameter");
+	}
+	public AA1(int x)
+	{
+		System.out.println("With integer type parameter");
+	}
+	public AA1(String x)
+	{
+		System.out.println("with string type parameter");
+	}
+
+}
---
+++ AA1K2.java
@@ -0,0 +1,11 @@
+
+public class AA1K2 {
+
+	public static void main(String[] args) {
+		new AA1();
+		new AA1(12);
+		new AA1("Adnan");
+
+	}
+
+}
