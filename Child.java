29d4156b12f41618d4d54d9d9d5fd0341be16817
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-12 23:18:25 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,13 @@
+public class Child 
+{
+	//constructor
+	int yearofbirth;
+	String Nameofchild;
+	//constructor create
+	public Child(int year, String name)
+	{
+		yearofbirth=year;
+		Nameofchild=name;
+	}
+	
+}
---
+++ ChildMain.java
@@ -0,0 +1,11 @@
+public class ChildMain 
+{
+	public static void main(String[] args) 
+	{
+	
+		//value assign to the constructors parameter
+		Child obj= new Child(2003,"Adnan Siddiqui");
+		System.out.println("Name of the child :  " +obj.Nameofchild+  "  and    Year of birth : "+obj.yearofbirth);
+	}
+
+}
